0c6823dd51feb6930a77999b61fd5919a0976c0e
David Martínez Carpena <dvmcarpena@pm.me>
2025-07-09 12:16:33 +0200

    Refactor frontend to use Svelte

---
+++ frontend/src/App.svelte
@@ -0,0 +1,69 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+
+  const apiUrl = "http://127.0.0.1:5001/";
+  
+  let data = $state();
+  let number = $state();
+
+	async function roll() {
+		const response = await fetch(apiUrl);
+		data = await response.json();
+    number = data["cohomology"]["aeppli"]["(1, 1)"];
+	}
+
+  $inspect(data)
+</script>
+
+<main>
+  <div>
+    <a href="https://vite.dev" target="_blank" rel="noreferrer">
+      <img src={viteLogo} class="logo" alt="Vite Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    </a>
+  </div>
+  <h1>Vite + Svelte</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+
+  
+  <h1>Calculator</h1>
+  <p>Kodaira-Thurston manifold</p>
+  <button onclick={roll}>Roll the dice</button>
+
+  {#if number !== undefined}
+    <p>You rolled a {number}</p>
+  {/if}
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+  </p>
+
+  <p class="read-the-docs">
+    Click on the Vite and Svelte logos to learn more
+  </p>
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
