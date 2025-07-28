7a43c4e5efe783037fcab4be1c7a0608d803edbf
David Martínez Carpena <dvmcarpena@pm.me>
2025-07-28 15:14:20 +0200

    First version frontend with only one cohomology of one input

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -2,68 +2,171 @@
   import svelteLogo from './assets/svelte.svg'
   import viteLogo from '/vite.svg'
   import Counter from './lib/Counter.svelte'
+  import KT from './assets/KT.json'
 
-  const apiUrl = "http://127.0.0.1:5001/";
+  // const apiUrl = "http://127.0.0.1:5001/";
   
   let data = $state();
   let number = $state();
+  let n = $state();
 
 	async function roll() {
-		const response = await fetch(apiUrl);
-		data = await response.json();
     number = data["cohomology"]["aeppli"]["(1, 1)"];
 	}
+
+  function a() {
+    n = 2;
+  }
+
+  function compare(a, b){
+    const ka = a[0].substring(1, a[0].length - 1).split(",").map(s => parseInt(s.trim()));
+    const kb = b[0].substring(1, b[0].length - 1).split(",").map(s => parseInt(s.trim()));
+    if (ka[1] > kb[1]) {
+      return -1;
+    } else if (ka[1] < kb[1]) {
+      return 1;
+    } else if (ka[0] < kb[0]) {
+      return -1
+    } else if (ka[0] > kb[0]) {
+      return 1;
+    }
+    return 0;
+    // if (ka[1] < kb[1]) {
+    //   return -1;
+    // } else if (ka[1] > kb[1]) {
+    //   return 1;
+    // } else if (ka[0] > kb[0]) {
+    //   return -1
+    // } else if (ka[0] < kb[0]) {
+    //   return 1;
+    // }
+    // return 0;
+  }
+
+  // const response = await fetch(apiUrl);
+  // data = await response.json();
+  data = KT;
+  n = data["n"];
 
   $inspect(data)
 </script>
 
-<main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+<main style="--n: {n}">
+  <div id="top-bar">
+    <div class="content">
+      <h1>bbCalculator</h1>
+    </div>
   </div>
-  <h1>Vite + Svelte</h1>
+  <div class="content">
+    <div id="input">
+      
+      <p>n = </p>
+      <label>
+        <input type="number" bind:value={n} min="0" />
+        <input type="range" bind:value={n} min="0" />
+      </label>
 
-  <div class="card">
-    <Counter />
+      <div>
+        <a href="https://vite.dev" target="_blank" rel="noreferrer">
+          <img src={viteLogo} class="logo" alt="Vite Logo" />
+        </a>
+      </div>
+
+      <div class="card">
+        <Counter />
+      </div>
+
+      
+      <p>Kodaira-Thurston manifold</p>
+      <button onclick={roll}>Roll the dice</button>
+
+      {#if number !== undefined}
+        <p>You rolled a {number}</p>
+      {/if}
+    </div>
+    <div id="output">
+      <!-- TODO reorder list to mach expected table of cohomology -->
+      {#each Object.entries(data["cohomology"]["dell"]).sort(compare) as [key, value]}
+        {@const dim = key.substring(1, key.length - 1).split(",").map(b => parseInt(b.trim()))}
+        {#if dim[0] == 0}
+          <div>{dim[1]}</div>
+        {/if}
+        <div>{key} {value}</div>
+      {/each}
+      <div></div>
+      {#each [...Array(n).keys()] as key}
+        <div>{key}</div>
+      {/each}
+    </div>
   </div>
-
-  
-  <h1>Calculator</h1>
-  <p>Kodaira-Thurston manifold</p>
-  <button onclick={roll}>Roll the dice</button>
-
-  {#if number !== undefined}
-    <p>You rolled a {number}</p>
-  {/if}
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
+  main {
+    margin: 0;
+    display: flex;
+    flex-direction: column;
+    place-items: center;
+    min-width: 320px;
+    min-height: 100vh;
+  }
+
+  #output {
+    width: 60%;
+    margin: 1rem;
+    display: grid;
+    grid-template-columns: 1fr repeat(var(--n), 3fr);
+    grid-template-rows: repeat(var(--n), 3fr) 1fr;
+    padding: 10px;
+    align-items: center;
+    justify-items: center;
+    /* aspect-ratio: 1 / 1; */
+  }
+
+  #output > div {
+    border: 1px black dashed;
+    padding: 10px;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  #input {
+    width: 40%;
+    border-right: 1px black solid;
+  }
+
+  main > .content {
+    padding: 2rem;
+    display: flex;
+  }
+
+  #top-bar {
+    width: 100vw;
+    background-color: darkslateblue;
+    /* height: 5em; */
+    color: white;
+    margin-bottom: 1.5rem;
+  }
+
+  .content {
+    max-width: 1280px;
+    width: 90%;
+    margin: 0 auto;
+    text-align: center;
+  }
+
   .logo {
     height: 6em;
     padding: 1.5em;
     will-change: filter;
     transition: filter 300ms;
   }
+
   .logo:hover {
     filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
   }
 </style>
