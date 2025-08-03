fd5bcc8b8fe178ceffe3b891536c0d2d4612e9c1
David Martínez Carpena <dvmcarpena@pm.me>
2025-08-03 22:58:10 +0200

    Refactor header and app

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,24 +1,30 @@
 <script>
+  import favicon from '../favicon.svg';
   import Input from './lib/Input.svelte';
   import Output from './lib/Output.svelte';
 
   let data = $state();
 </script>
 
-<main>
-  <div id="top-bar">
+<div id="app">
+  <header>
     <div class="content">
-      <h1>bbCalculator</h1>
+      <a href="/" target="_blank" rel="noreferrer">
+        <img src={favicon} class="logo" alt="bbCalculator" />
+      </a>
+      <a href="/" target="_blank" rel="noreferrer">
+        <h1>bbCalculator</h1>
+      </a>
     </div>
-  </div>
-  <div class="content">
+  </header>
+  <main class="content">
     <Input bind:data />
     <Output {data} />
-  </div>
-</main>
+  </main>
+</div>
 
 <style>
-  main {
+  #app {
     margin: 0;
     display: flex;
     flex-direction: column;
@@ -27,23 +33,44 @@
     min-height: 100vh;
   }
 
-  main > .content {
-    padding: 2rem;
-    display: flex;
+  header {
+    flex: 0 1 auto;
+    width: 100vw;
+    /* background-color: darkslateblue; */
+    /* height: 5em; */
+    /* color: white; */
+    /* margin-bottom: 1.5rem; */
+    box-shadow: 0 2px 2px 0 rgba(0,0,0,.14),0 1px 5px 0 rgba(0,0,0,.12),0 3px 1px -2px rgba(0,0,0,.2);
   }
 
-  #top-bar {
-    width: 100vw;
-    background-color: darkslateblue;
-    /* height: 5em; */
-    color: white;
-    margin-bottom: 1.5rem;
+  main {
+    flex: 1 1 auto;
+    display: flex;
   }
 
   .content {
     max-width: 1280px;
-    width: 90%;
-    margin: 0 auto;
-    text-align: center;
+    width: 100%;
+  }
+
+  header > .content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: auto;
+  }
+
+  header h1 {
+    font-family: "Montserrat", sans-serif;
+    font-optical-sizing: auto;
+    font-weight: 700;
+    font-style: normal;
+    letter-spacing: 3px;
+    color: rgb(90, 90, 90);
+  }
+
+  header img {
+    height: 3rem;
+    margin-right: 1.5rem;
   }
 </style>
