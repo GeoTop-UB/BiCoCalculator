1352f37a0dc538afb1a19ddc47ba73aba23ca961
David Martínez Carpena <dvmcarpena@pm.me>
2025-08-03 12:27:21 +0200

    Move Output to component

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,68 +1,20 @@
 <script>
   // import svelteLogo from './assets/svelte.svg'
   import viteLogo from '/vite.svg'
-  import TableOutput from './lib/TableOutput.svelte'
+  import Output from './lib/Output.svelte';
   import KT from './assets/KT.json'
   import { math } from 'mathlifier';
+    
   // import { draw } from 'svelte/transition';
 
   // const apiUrl = "http://127.0.0.1:5001/";
-  
-  const tabs = [
-    {
-      "id": "cohomology_aeppli",
-      "name": "Aeppli Cohomology",
-      "type": "cohomology",
-    },
-    {
-      "id": "cohomology_bottchern",
-      "name": "Bottchern Cohomology",
-      "type": "cohomology",
-    },
-    {
-      "id": "cohomology_delbar",
-      "name": "Delbar Cohomology",
-      "type": "cohomology",
-    },
-    {
-      "id": "cohomology_dell",
-      "name": "Dell Cohomology",
-      "type": "cohomology",
-    },
-    {
-      "id": "cohomology_reduced_aeppli",
-      "name": "Reduced Aeppli Cohomology",
-      "type": "cohomology",
-    },
-    {
-      "id": "cohomology_reduced_bottchern",
-      "name": "Reduced Bottchern Cohomology",
-      "type": "cohomology",
-    },
-    {
-      "id": "zigzags",
-      "name": "Zigzags",
-      "type": "zigzags",
-    },
-    {// TODO
-      "id": "squares",
-      "name": "Squares",
-      "type": "cohomology",
-    }
-  ];
+
   let data = $state();
   let number = $state();
   let n = $state();
-  let tab = $state(tabs[0]["id"])
-  let type = $state(tabs[0]["type"])
 
 	async function roll() {
     number = data["cohomology_aeppli"]["(1, 1)"];
-	}
-
-	async function changeTab(t) {
-    tab = t.id;
-    type = t.type;
 	}
 
   // const response = await fetch(apiUrl);
@@ -114,15 +66,7 @@
         <p>You rolled a {number}</p>
       {/if}
     </div>
-    <div id="output">
-      <div class="tab">
-        {#each tabs as t}
-          <button class="tablinks" onclick={() => changeTab(t)}>{t.name}</button>
-        {/each}
-      </div>
-
-      <TableOutput {data} {n} {type} {tab} />
-    </div>
+    <Output {data} {n} />
   </div>
 </main>
 
@@ -134,11 +78,6 @@
     place-items: center;
     min-width: 320px;
     min-height: 100vh;
-  }
-
-  #output {
-    width: 60%;
-    margin: 1rem;
   }
 
   #input {
@@ -175,32 +114,5 @@
 
   .logo:hover {
     filter: drop-shadow(0 0 2em #646cffaa);
-  }
-
-  .tab {
-    overflow: hidden;
-    border: 1px solid #ccc;
-    background-color: #f1f1f1;
-  }
-
-  /* Style the buttons that are used to open the tab content */
-  .tab button {
-    background-color: inherit;
-    float: left;
-    border: none;
-    outline: none;
-    cursor: pointer;
-    padding: 14px 16px;
-    transition: 0.3s;
-  }
-
-  /* Change background color of buttons on hover */
-  .tab button:hover {
-    background-color: #ddd;
-  }
-
-  /* Create an active/current tablink class */
-  .tab button.active {
-    background-color: #ccc;
   }
 </style>
---
+++ frontend/src/lib/Output.svelte
@@ -0,0 +1,100 @@
+<script>
+  import TableOutput from './TableOutput.svelte'
+  
+  const tabs = [
+    {
+      "id": "cohomology_aeppli",
+      "name": "Aeppli Cohomology",
+      "type": "cohomology",
+    },
+    {
+      "id": "cohomology_bottchern",
+      "name": "Bottchern Cohomology",
+      "type": "cohomology",
+    },
+    {
+      "id": "cohomology_delbar",
+      "name": "Delbar Cohomology",
+      "type": "cohomology",
+    },
+    {
+      "id": "cohomology_dell",
+      "name": "Dell Cohomology",
+      "type": "cohomology",
+    },
+    {
+      "id": "cohomology_reduced_aeppli",
+      "name": "Reduced Aeppli Cohomology",
+      "type": "cohomology",
+    },
+    {
+      "id": "cohomology_reduced_bottchern",
+      "name": "Reduced Bottchern Cohomology",
+      "type": "cohomology",
+    },
+    {
+      "id": "zigzags",
+      "name": "Zigzags",
+      "type": "zigzags",
+    },
+    {// TODO
+      "id": "squares",
+      "name": "Squares",
+      "type": "cohomology",
+    }
+  ];
+
+  let { data, n } = $props();
+  
+  let tab = $state(tabs[0]["id"])
+  let type = $state(tabs[0]["type"])
+
+	async function changeTab(t) {
+    tab = t.id;
+    type = t.type;
+	}
+</script>
+
+<div id="output">
+  <div class="tab">
+  {#each tabs as t}
+    <button class="tablinks" onclick={() => changeTab(t)}>{t.name}</button>
+  {/each}
+  </div>
+
+  <TableOutput {data} {n} {type} {tab} />
+</div>
+
+<style>
+  #output {
+    width: 60%;
+    margin: 1rem;
+  }
+
+  .tab {
+    overflow: hidden;
+    border: 1px solid #ccc;
+    background-color: #f1f1f1;
+  }
+
+  /* Style the buttons that are used to open the tab content */
+  .tab button {
+    background-color: inherit;
+    float: left;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    padding: 14px 16px;
+    transition: 0.3s;
+  }
+
+  /* Change background color of buttons on hover */
+  .tab button:hover {
+    background-color: #ddd;
+  }
+
+  /* Create an active/current tablink class */
+  .tab button.active {
+    background-color: #ccc;
+  }
+</style>
