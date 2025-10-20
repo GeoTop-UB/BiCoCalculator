68d2c9c1a47ba03ce5cafe8241e4a8c07db33be6
David Martínez Carpena <dvmcarpena@pm.me>
2025-10-20 10:42:19 +0200

    Fix resolve url

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -25,7 +25,7 @@
       <a href="{resolve('/')}" target="_blank" rel="noreferrer">
         <img src={favicon} class="logo" alt="bbCalculator" />
       </a>
-      <a href="{resolve('/')}/" target="_blank" rel="noreferrer">
+      <a href="{resolve('/')}" target="_blank" rel="noreferrer">
         <h1>bbCalculator</h1>
       </a>
     </div>
