0b4f660e5b443f1be9e4e26412c646baa9e476ec
David Martínez Carpena <dvmcarpena@pm.me>
2025-11-13 14:23:32 +0100

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/env bash
 
 # make build-static
-export $(grep -v '^#' .env.secrets | xargs -d '\n')
+set -o allexport
+source .env.secrets
+set +o allexport
+# export $(grep -v '^#' .env.secrets | xargs -d '\n')
+# mkdir bicocalculator
+# mkdir _app
+# mkdir _app/immutable
+# mkdir _app/immutable/assets
+# mkdir _app/immutable/chunks
+# mkdir _app/immutable/entry
+# mkdir _app/immutable/nodes
 sshpass -p $DEPLOY_PASSWORD sftp -oBatchMode=no -b - $DEPLOY_HOST << !
    cd bicocalculator
    rm _app/immutable/assets/*
    rm _app/immutable/chunks/*
    rm _app/immutable/entry/*
    rm _app/immutable/nodes/*
-   put -R build/* .
+   put -R _build/* .
    bye
 !
